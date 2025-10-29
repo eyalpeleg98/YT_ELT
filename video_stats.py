794f009ee2079faaaecf19fe4cff44f0c5af65f1
eyal.peleg <eyal.peleg@4m-a.com>
2025-10-29 14:47:13 +0200

    space cleaning

--- video_stats.py
+++ video_stats.py
@@ -10,7 +10,6 @@ CHANNEL_HANDLE = 'MrBeast'
 MaxResults = 50
 
 def get_playlist_id():
-
     try:
         url = f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}"
         response = requests.get(url)
@@ -20,8 +19,6 @@ def get_playlist_id():
         channel_playlistId = channel_items["contentDetails"]["relatedPlaylists"]["uploads"]
         # print(f"Playlist ID: {channel_playlistId}")
         return channel_playlistId
-
-
     except requests.exceptions.requestException as e:
         raise e
 
@@ -40,7 +37,6 @@ def get_video_ids(playlist_id):
             for item in data.get('items', []):
                 video_id = item['contentDetails']['videoId']
                 video_ids.append(video_id)
-
             page_token = data.get('nextPageToken')
             if not page_token:
                 break
@@ -50,21 +46,14 @@ def get_video_ids(playlist_id):
         raise e
 
 
-
-
-
-
 def extract_video_data(video_ids):
     extracted_data = []
-
     def batch_list(video_id_lst, batch_size):
         for video_id in range(0, len(video_id_lst), batch_size):
             yield video_id_lst[video_id:video_id + batch_size]
-
     try:
         for batch in batch_list(video_ids, MaxResults):
             video_ids_str = ','.join(batch)
-
             url = f'https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&part=snippet&part=statistics&id=yWEUL3crU_E&key={API_KEY}'
             response = requests.get(url)
             response.raise_for_status()
@@ -74,7 +63,6 @@ def extract_video_data(video_ids):
                 snippet = item['snippet']
                 contentDetails = item['contentDetails']
                 statistics = item['statistics']
-
                 video_data = {
                     "video_id": video_id,
                     "title": snippet['title'],
@@ -84,7 +72,6 @@ def extract_video_data(video_ids):
                     "likeCount": statistics.get('likeCount', None),
                     "commentCount": statistics.get('commentCount', None)
                 }
-
                 extracted_data.append(video_data)
         return extracted_data
     except requests.exceptions.RequestException as e:
